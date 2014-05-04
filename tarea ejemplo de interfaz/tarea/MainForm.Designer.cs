5e42bf23fefbdf0fc4a3242d2d6035e5d2aaf039
OmarAraujo <omar-553@hotmail.com>
2014-05-04 02:13:07 -0500

    Esta es una tarea que dejo, consiste en hacer solamente la interfaz de un usuario y su password, un inicio de sesion.

---
+++ tarea ejemplo de interfaz/tarea/MainForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ing. Omar
+ * Date: 08/04/2014
+ * Time: 22:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace tarea
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.password = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// password
+			// 
+			this.password.Location = new System.Drawing.Point(69, 87);
+			this.password.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
+			this.password.Name = "password";
+			this.password.PasswordChar = '*';
+			this.password.Size = new System.Drawing.Size(90, 20);
+			this.password.TabIndex = 1;
+			this.password.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(69, 123);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(96, 35);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Iniciar sesion";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(69, 58);
+			this.label2.Margin = new System.Windows.Forms.Padding(3, 9, 3, 14);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(90, 23);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Password";
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(69, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Usuario";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(69, 35);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 5;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Blue;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.password);
+			this.Name = "MainForm";
+			this.Text = "tarea";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox password;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ tarea ejemplo de interfaz/tarea/MainForm.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ing. Omar
+ * Date: 08/04/2014
+ * Time: 22:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tarea
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
